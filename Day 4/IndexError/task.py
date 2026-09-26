03d6667a3902d9b41e03316880483c5297ac491c
ArtWarrior6905 <tejshree.tanuj@gmail.com>
2026-09-26 21:52:20 +0530

    Rock Paper Scissors

--- Day 4/IndexError/task.py
+++ Day 4/IndexError/task.py
@@ -1,9 +1,4 @@
-states_of_america = ["Delaware", "Pennsylvania", "New Jersey", "Georgia", "Connecticut", "Massachusetts", "Maryland",
-                     "South Carolina", "New Hampshire", "Virginia", "New York", "North Carolina", "Rhode Island",
-                     "Vermont", "Kentucky", "Tennessee", "Ohio", "Louisiana", "Indiana", "Mississippi", "Illinois",
-                     "Alabama", "Maine", "Missouri", "Arkansas", "Michigan", "Florida", "Texas", "Iowa", "Wisconsin",
-                     "California", "Minnesota", "Oregon", "Kansas", "West Virginia", "Nevada", "Nebraska", "Colorado",
-                     "North Dakota", "South Dakota", "Montana", "Washington", "Idaho", "Wyoming", "Utah", "Oklahoma",
-                     "New Mexico", "Arizona", "Alaska", "Hawaii"]
-
-print(states_of_america)
+a = [1,2,3,4]
+b = [5,6,7,8]
+c = [a,b]
+print(c[1][1])
--- Day 4/Rock Paper Scissors/task.py
+++ Day 4/Rock Paper Scissors/task.py
@@ -1,3 +1,4 @@
+import random
 rock = '''
     _______
 ---'   ____)
@@ -24,3 +25,20 @@ scissors = '''
       (____)
 ---.__(___)
 '''
+a = int(input("To pick rock type '0', paper type '1', scissors type '2':\n "))
+print("You chose: ",a)
+if a == 0:
+    print(rock)
+elif a == 1:
+    print(paper)
+elif a == 2:
+    print(scissors)
+b = [rock,paper,scissors]
+c=random.choice(b)
+print("Computer chose: ",c)
+if (a == 0 and c == scissors) or (a == 1 and c == rock) or (a == 2 and c == paper):
+    print("You win!")
+elif (a == 0 and c == paper) or (a == 1 and c == scissors) or (a == 2 and c == rock):
+    print("You lose!")
+else:
+    print("Draw")
